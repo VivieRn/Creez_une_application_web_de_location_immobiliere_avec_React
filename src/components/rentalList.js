d694407b2e2eb832a43a05bf97fcc2083333c3e9
Nicolas VIVIER <vivier.nicolas0@gmail.com>
2023-08-23 13:26:16 +0200

    Révision du Responsive

--- src/components/rentalList.js
+++ src/components/rentalList.js
@@ -8,15 +8,17 @@ import "../style/rentalList.scss";
 function RentalList() {
   return (
     <div className="RentalList">
-      <ul className="flat-list">
-        {flatList.map((flat) => (
-          <Link to={`/flat/${flat.id}`} key={flat.id}>
-            <li className="flat-item">
-              <h2>{flat.title}</h2>
-            </li>
-          </Link>
-        ))}
-      </ul>
+      <div className="ctn-flat-list">
+        <ul className="flat-list">
+          {flatList.map((flat) => (
+            <Link to={`/flat/${flat.id}`} key={flat.id}>
+              <li className="flat-item">
+                <h2>{flat.title}</h2>
+              </li>
+            </Link>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
