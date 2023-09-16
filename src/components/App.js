3f38e576ea58aca26c93f448e540bd67e6adc795
Nicolas VIVIER <vivier.nicolas0@gmail.com>
2023-09-16 12:00:51 +0200

    Ajustement des routes

--- src/components/App.js
+++ src/components/App.js
@@ -35,7 +35,7 @@ function App() {
               </>
             }
           />
-          <Route path="/*" element={<NotFound />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
         <Footer />
       </div>
